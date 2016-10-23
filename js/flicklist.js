cc957f703d512d5e84fc168f12c307d1e266c949
Anthony Stark <anthony.stark@equifax.com>
2016-10-23 00:43:06 -0500

    Safety - Stable UI

--- js/flicklist.js
+++ js/flicklist.js
@@ -17,6 +17,35 @@ var api = {
 
 
 
+function initializePage()
+{
+    //$("#form-search").submit(function(evt) {
+    //    evt.preventDefault();
+    //    var query = $("#form-search input[name=query]").val();
+    //    searchMovies(query, render);
+    //});
+
+    //// DONE
+    //// add event handler to the "Add to Watchlist" button
+    //$("#add-to-watchlist").click(function() {
+    //    addActiveMovie();
+    //    render();
+    //});
+
+    //// DONE
+    //// add event handler for when the carousel has slid
+    //$("#browse-carousel").bind("slid.bs.carousel", function() {
+    //    console.log("slid");
+    //    var newIndex = $("#browse-carousel").find(".active").index();
+    //    console.log(newIndex);
+    //    model.browseActiveIndex = newIndex;
+    //    render();
+    //});
+
+    // initial fetch
+    searchMovies("", render);
+}
+
 /**
  * Makes an AJAX request to the /discover endpoint of the API, using the 
  * keyword ID that was passed in
@@ -88,60 +117,61 @@ function fetchKeywords(query, cbSuccess, cbError) {
  * re-renders the page with new content, based on the current state of the model
  */
 function render() {
-  var watchlistElement = $("#section-watchlist ul");
-  var carouselInner = $("#section-browse .carousel-inner");
-  var browseInfo = $("#browse-info");
+  //var watchlistElement = $("#section-watchlist ul");
+  //var carouselInner = $("#section-browse .carousel-inner");
+  //var browseInfo = $("#browse-info");
 
-  // clear everything
-  watchlistElement.empty();
-  carouselInner.empty();
-  browseInfo.empty();
+  //// clear everything
+  //watchlistElement.empty();
+  //carouselInner.empty();
+  //browseInfo.empty();
 
-  // insert watchlist items
-  model.watchlistItems.forEach(function(movie) {
+  //// insert watchlist items
+  //model.watchlistItems.forEach(function(movie) {
 
-    // panel heading
-    var title = $("<h5></h5>").text(movie.original_title);  
-    var panelHeading = $("<div></div>")
-      .attr("class", "panel-heading")
-      .append(title);
+  //  // panel heading
+  //  var title = $("<h5></h5>").text(movie.original_title);  
+  //  var panelHeading = $("<div></div>")
+  //    .attr("class", "panel-heading")
+  //    .append(title);
 
-    // panel body
-    var poster = $("<img></img>")
-      .attr("src", posterUrl(movie, "w300"));
-    var panelBody = $("<div></div>")
-      .attr("class", "panel-body")
-      .append(poster)
-      .append(button);
+  //  // panel body
+  //  var poster = $("<img></img>")
+  //    .attr("src", posterUrl(movie, "w300"));
+  //  var panelBody = $("<div></div>")
+  //    .attr("class", "panel-body")
+  //    .append(poster)
+  //    .append(button);
 
-    // panel
-    var panel = $("<div></div>")
-      .attr("class", "panel panel-default")
-      .append(panelHeading)
-      .append(panelBody);
+  //  // panel
+  //  var panel = $("<div></div>")
+  //    .attr("class", "panel panel-default")
+  //    .append(panelHeading)
+  //    .append(panelBody);
 
 
-    var button = $("<button></button>")
-      .text("I watched it")
-      .attr("class", "btn")
-      .click(function() {
-        removeFromWatchlist(movie);
-        render();
-      })
-      .hide();
+  //  var button = $("<button></button>")
+  //    .text("I watched it")
+  //    .attr("class", "btn")
+  //    .click(function() {
+  //      removeFromWatchlist(movie);
+  //      render();
+  //    })
+  //    .hide();
 
-    var itemView = $("<li></li>")
-      .append(panel)
-      .append(button)
-      .mouseover(function() {
-        button.show();
-      })
-      .mouseleave(function() {
-        button.hide();
-      });
+  //  var itemView = $("<li></li>")
+  //    .append(panel)
+  //    .append(button)
+  //    .mouseover(function() {
+  //      button.show();
+  //    })
+  //    .mouseleave(function() {
+  //      button.hide();
+  //    });
 
-    watchlistElement.append(itemView)
-  });
+  //  watchlistElement.append(itemView)
+  //});
+
 
   // insert browse items
   model.browseItems.forEach(function(movie, index) {
@@ -151,37 +181,52 @@ function render() {
     // wrap the image inside a div
     // append the item into the carousel-inner element
 
-    var poster = $("<img></img>")
-      .attr("src", posterUrl(movie, "w300"));
-    var carouselItem = $("<div></div>")
-      .attr("class", "item")
-      .append(poster);
-    
-    carouselInner.append(carouselItem);
+      //movieCarousel
+
+      //<a class="carousel-item" href="#one!"><img src="netflix-logo-gray-3d.png"></a>
+
+
+
+      var poster = $("<img></img>")
+  .attr("src", posterUrl(movie, "w300"));
+
+      var carouselItem = $("<a></a>")
+        .attr("class", "carousel-item")
+        .attr("href", "#" + index)
+        .append(poster);
+
+      $("#movieCarousel").append(carouselItem);
+
+
 
     // DONE
     // if this index is the current active index,
     // give this item a class attribute of "active"
-    if (index === model.browseActiveIndex) {
-      carouselItem.attr("class", "item active");
-    }
+    //if (index === model.browseActiveIndex) {
+    //  carouselItem.attr("class", "item active");
+    //}
 
   });
 
-  // DONE display info for the currently active movie
-  var activeMovie = model.browseItems[model.browseActiveIndex];
-  var title = $("<h3></h3>").text(activeMovie.original_title);
-  var overview = $("<p></p>").text(activeMovie.overview);
-  browseInfo
-    .append(title)
-    .append($("<hr/>"))
-    .append(overview);
+  $('.carousel').removeClass("initialized");
+
+    // material design Carousel Initialization
+  $('.carousel').carousel();
+
+  //// DONE display info for the currently active movie
+  //var activeMovie = model.browseItems[model.browseActiveIndex];
+  //var title = $("<h3></h3>").text(activeMovie.original_title);
+  //var overview = $("<p></p>").text(activeMovie.overview);
+  //browseInfo
+  //  .append(title)
+  //  .append($("<hr/>"))
+  //  .append(overview);
   
-  // DONE
-  // disable or enable the Add to Watchlist button depending on
-  // whether the current active movie is already on the user's watchlist
-  var alreadyOnWatchlist = model.watchlistItems.indexOf(activeMovie) !== -1
-  $("#add-to-watchlist").prop("disabled", alreadyOnWatchlist);
+  //// DONE
+  //// disable or enable the Add to Watchlist button depending on
+  //// whether the current active movie is already on the user's watchlist
+  //var alreadyOnWatchlist = model.watchlistItems.indexOf(activeMovie) !== -1
+  //$("#add-to-watchlist").prop("disabled", alreadyOnWatchlist);
 }
 
 
